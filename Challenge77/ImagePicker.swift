1b546060ef5540f191dd6e8e718cd28368971be8
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-09 11:55:05 +0000

    Adds image Picker

--- Challenge77/ImagePicker.swift
+++ Challenge77/ImagePicker.swift
@@ -11,11 +11,20 @@ import UIKit
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedImage: Image?
+    
+    ///Creates a coordinator class that acts as a delegate for the image picker struct
     class Coordinator: NSObject , UINavigationControllerDelegate , UIImagePickerControllerDelegate{
         let parent: ImagePicker
         
         init(_ parent: ImagePicker) {
             self.parent = parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let selectedImage = info[.originalImage] as? UIImage else{ return}
+            parent.selectedImage = Image(uiImage: selectedImage)
+            parent.presentationMode.wrappedValue.dismiss()
         }
     }
     
